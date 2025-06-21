9495863cd82c766a2d0042bd1d283abacc3c437a
Om Zalavadiya <omzalavadiya301002@gmail.com>
2025-06-21 11:32:13 +0530

    Used PUT and DELETE methods with method-override

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const methodOverride = require("method-override");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -7,9 +8,10 @@ let todos = [];
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(methodOverride("_method")); // override method based on query param or hidden input
 
 app.get("/", (req, res) => {
-  res.render("list", { todos });
+  res.render("list", { todos, error: req.query.error });
 });
 
 app.post("/add", (req, res) => {
@@ -19,18 +21,18 @@ app.post("/add", (req, res) => {
   res.redirect("/");
 });
 
-app.post("/delete", (req, res) => {
-  todos = todos.filter(todo => todo.id !== Number(req.body.id));
-  res.redirect("/");
-});
-
-app.post("/edit", (req, res) => {
-  const { id, task, priority } = req.body;
-  const todo = todos.find(todo => todo.id === Number(id));
+app.put("/edit/:id", (req, res) => {
+  const { task, priority } = req.body;
+  const todo = todos.find(todo => todo.id === Number(req.params.id));
   if (todo) {
     todo.task = task;
     todo.priority = priority;
   }
+  res.redirect("/");
+});
+
+app.delete("/delete/:id", (req, res) => {
+  todos = todos.filter(todo => todo.id !== Number(req.params.id));
   res.redirect("/");
 });
 
